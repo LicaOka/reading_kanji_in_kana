5fd2f398c59cb96d48953f294aa45ca2b897a11d
Lica Okamoto <lica@nii.ac.jp>
2017-08-10 21:22:46 +0900

    [wip]first commit

---
+++ lib/reading_kanji_in_kana.rb
@@ -0,0 +1,7 @@
+require "reading_kanji_in_kana/version"
+
+require "natto"
+
+module ReadingKanjiInKana
+  # Your code goes here...
+end
---
+++ lib/reading_kanji_in_kana/version.rb
@@ -0,0 +1,3 @@
+module ReadingKanjiInKana
+  VERSION = "0.1.0"
+end
